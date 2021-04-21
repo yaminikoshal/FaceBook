7a9cc7c0400856190795f00e819786a286e6707f
yaminikoshal <yaminiraveendran1@gmail.com>
2021-04-21 18:26:10 +0530

    updated coding from yamini

--- src/org/git/Sample.java
+++ src/org/git/Sample.java
@@ -3,6 +3,9 @@ package org.git;
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("completed");
+		
+		System.out.println("Updated from master to own");
 	}
+	
 
 }
